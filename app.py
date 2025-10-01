a7546cc40b3b988c7347ee8d817a087f527ef290
deepakpro190 <deepak.goel.pro@gmail.com>
2025-10-01 16:03:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,10 +64,10 @@ chain = None
 retriever = None
 rebuild_lock = threading.Lock()
 
-origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[*],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
