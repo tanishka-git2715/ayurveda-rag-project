2d7b787656c534c484f502ece67aeb6b39c5a0cf
Tanishka-tech <tashukhandelwal27@gmail.com>
2025-10-05 22:12:03 +0530

    commit

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if not api_key:
 groq_api_key = os.getenv("GROQ_API_KEY")
 client = Groq(api_key=groq_api_key)
 
-DATA_PATH = "data/ayurveda_books/"
+DATA_PATH = "data/"
 VECTOR_STORE_PATH = "vectorstore/"
 
 chain = None
